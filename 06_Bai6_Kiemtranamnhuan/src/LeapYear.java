e92c92793c034586bf1ddaed33f070fd064b34ef
tiennv0608 <tiennv0608@gmail.com>
2021-05-05 17:33:33 +0700

    bai6

---
+++ 06_Bai6_Kiemtranamnhuan/src/LeapYear.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = sc.nextInt();
+
+        boolean isLeapYear = false;
+        boolean isDivisibleBy4 = year % 4 ==0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 ==0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 ==0;
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.println(year+ " is a leap year!");
+        } else {
+            System.out.println(year+ " is Not a leap year!");
+        }
+    }
+}
