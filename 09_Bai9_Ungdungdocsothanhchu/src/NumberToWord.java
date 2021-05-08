a24f2a117030aa2ad85b6ffe61a73896c7ee7525
tiennv0608 <tiennv0608@gmail.com>
2021-05-08 12:27:38 +0700

    b9

--- 09_Bai9_Ungdungdocsothanhchu/src/NumberToWord.java
+++ 09_Bai9_Ungdungdocsothanhchu/src/NumberToWord.java
@@ -2,74 +2,163 @@ import java.util.Scanner;
 
 public class NumberToWord {
     public static void main(String[] args) {
-        String str;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int number = scanner.nextInt();
-        switch (number) {
-            case 0:
-                str = "zero";
-                break;
-            case 1:
-                str = "one";
-                break;
-            case 2:
-                str = "two";
-                break;
-            case 3:
-                str = "three";
-                break;
-            case 4:
-                str = "four";
-                break;
-            case 5:
-                str = "five";
-                break;
-            case 6:
-                str = "six";
-                break;
-            case 7:
-                str = "seven";
-                break;
-            case 8:
-                str = "eight";
-                break;
-            case 9:
-                str = "nine";
-                break;
-            case 10:
-                str = "ten";
-                break;
-            case 11:
-                str = "eleven";
-                break;
-            case 12:
-                str = "twelve";
-                break;
-            case 13:
-                str = "thirteen";
-                break;
-            case 14:
-                str = "fourteen";
-                break;
-            case 15:
-                str = "fifteen";
-                break;
-            case 16:
-                str = "sixteen";
-                break;
-            case 17:
-                str = "seventeen";
-                break;
-            case 18:
-                str = "eighteen";
-                break;
-            case 19:
-                str = "nineteen";
-                break;
-            default:
-                str = "Out of ability";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to read(0-1000):");
+        int x = sc.nextInt();
+        String readNum = "";
+        if (x < 0 || x >= 1000) {
+            System.out.println("out of ability");
+        } else {
+            switch (x / 100) {
+                case 1:
+                    readNum += "one hundred and ";
+                    break;
+                case 2:
+                    readNum += "two hundred and ";
+                    break;
+                case 3:
+                    readNum += "three hundred and ";
+                    break;
+                case 4:
+                    readNum += "four hundred and ";
+                    break;
+                case 5:
+                    readNum += "five hundred and ";
+                    break;
+                case 6:
+                    readNum += "six hundred and ";
+                    break;
+                case 7:
+                    readNum += "seven hundred and ";
+                    break;
+                case 8:
+                    readNum += "eight hundred and ";
+                    break;
+                case 9:
+                    readNum += "nine hundred and ";
+                    break;
+            }
+            switch (x % 100 / 10) {
+                case 2:
+                    readNum += "twenty ";
+                    break;
+                case 3:
+                    readNum += "thirty ";
+                    break;
+                case 4:
+                    readNum += "forty ";
+                    break;
+                case 5:
+                    readNum += "fifty ";
+                    break;
+                case 6:
+                    readNum += "sixty ";
+                    break;
+                case 7:
+                    readNum += "seventy ";
+                    break;
+                case 8:
+                    readNum += "eighty ";
+                    break;
+                case 9:
+                    readNum += "ninety ";
+                    break;
+                default:
+                    break;
+            }
+            if (x % 100 > 20) {
+                switch (x % 10) {
+                    case 1:
+                        readNum += "one";
+                        break;
+                    case 2:
+                        readNum += "two";
+                        break;
+                    case 3:
+                        readNum += "three";
+                        break;
+                    case 4:
+                        readNum += "four";
+                        break;
+                    case 5:
+                        readNum += "five";
+                        break;
+                    case 6:
+                        readNum += "six";
+                        break;
+                    case 7:
+                        readNum += "seven";
+                        break;
+                    case 8:
+                        readNum += "eight";
+                        break;
+                    case 9:
+                        readNum += "nine";
+                        break;
+                }
+            }
+            switch (x % 100) {
+                case 1:
+                    readNum += "one";
+                    break;
+                case 2:
+                    readNum += "two";
+                    break;
+                case 3:
+                    readNum += "three";
+                    break;
+                case 4:
+                    readNum += "four";
+                    break;
+                case 5:
+                    readNum += "five";
+                    break;
+                case 6:
+                    readNum += "six";
+                    break;
+                case 7:
+                    readNum += "seven";
+                    break;
+                case 8:
+                    readNum += "eight";
+                    break;
+                case 9:
+                    readNum += "nine";
+                    break;
+                case 10:
+                    readNum += "ten";
+                    break;
+                case 11:
+                    readNum += "eleven";
+                    break;
+                case 12:
+                    readNum += "twelve";
+                    break;
+                case 13:
+                    readNum += "thirteen";
+                    break;
+                case 14:
+                    readNum += "fourteen";
+                    break;
+                case 15:
+                    readNum += "fifteen";
+                    break;
+                case 16:
+                    readNum += "sixteen";
+                    break;
+                case 17:
+                    readNum += "seventeen";
+                    break;
+                case 18:
+                    readNum += "eighteen";
+                    break;
+                case 19:
+                    readNum += "nineteen";
+                    break;
+                default:
+                    readNum += "zero";
+            }
+            System.out.println("The number " + x + " is read as " + readNum);
         }
-        System.out.println(str);
     }
 }
